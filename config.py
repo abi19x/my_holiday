dae4418aa7f8a6078459829ec2052ff7bec93ae2
Abel YOHANES <k9ja1xwk@students.codeinstitute.net>
2025-05-01 12:45:54 +0000

    databse ports amended

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
     SESSION_TYPE = 'filesystem'
-    DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://your_db_user:your_db_password@localhost/holiday_planner')
+    DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://myuser:masteRh0512.@localhost:5433/mydb')
