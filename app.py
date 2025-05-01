43232cf0dc0dd6f5158c8c1cd70d5ebdfd1c3f3e
Abel YOHANES <k9ja1xwk@students.codeinstitute.net>
2025-05-01 13:14:11 +0000

    render template added to the project

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_session import Session
 from flask import flash
 from config import Config
@@ -9,16 +9,26 @@ from routes.bookings import bookings_bp
 from routes.dashboard import dashboard_bp
 from routes.admin import admin_bp
 
+#def create_app():
+ #   app = Flask(__name__)
+  #  app.config.from_object(Config)
+
+   # Session(app)
+    #init_db(app)
+
+    #app.register_blueprint(auth_bp)
+    #app.register_blueprint(bookings_bp, url_prefix='/bookings')
+    #app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
+    #app.register_blueprint(admin_bp, url_prefix='/admin')
+
+    #return app
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    Session(app)
-    init_db(app)
-
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(bookings_bp, url_prefix='/bookings')
-    app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
-    app.register_blueprint(admin_bp, url_prefix='/admin')
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
     return app
